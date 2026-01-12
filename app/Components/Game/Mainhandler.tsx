a2dc42037b01111f5e42f08794fec7fd47d9b407
reportJNG <hamzaremali10@gmail.com>
2026-01-12 11:40:37 -0500

    finshed main header logic

--- app/Components/Game/Mainhandler.tsx
+++ app/Components/Game/Mainhandler.tsx
@@ -1,21 +1,30 @@
 interface  Mainhandlerprops{
-
+//indicator
+level:number;
+//pause
+volume: number;
+setVolume: React.Dispatch<React.SetStateAction<number>>;
+sound: boolean;
+setSound: React.Dispatch<React.SetStateAction<boolean>>;
+leave: () => void;
 }
 import { BOSSES } from '@/app/Types/Boss';
 import styles from './Mainhandler.module.css';
 import Title from '../Title';
 import Pause from '../Pause';
 import { useState } from 'react';
-export default function Mainhandler(){
+export default function Mainhandler({level,volume, setVolume, sound, setSound, leave }:Mainhandlerprops){
     //title values
     const [fightindicator,setFightinficator]=useState<boolean>(false);
     const [testwinner,setTesetwinner]=useState<string>('');
-    const [bossname,setBossname]=useState<string>('');
+    //pause values 
+
     return(
         <div className={styles.container}>
 
             <div className={styles.header}>
-            <Title text={bossname} winornot={testwinner} endedfight={fightindicator}/>
+                <div className={styles.midscreentop}><Title text={BOSSES[level].name} winornot={testwinner} endedfight={fightindicator}/></div>
+                <div className={styles.righttopscrenn}><Pause volume={volume} setVolume={setVolume} sound={sound} setSound={setSound} leave={leave} /></div>
             </div>
 
 
