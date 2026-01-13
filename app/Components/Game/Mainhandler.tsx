35c7a4783f809a5f0183694a5d669e916fdeb976
reportJNG <hamzaremali10@gmail.com>
2026-01-12 19:43:56 -0500

    updated perfectly logic (not done yet)

--- app/Components/Game/Mainhandler.tsx
+++ app/Components/Game/Mainhandler.tsx
@@ -7,6 +7,8 @@ import Playerstats from '../Playerstats';
 import Actions from '../Actions';
 import { Player } from '@/app/Types/Player';
 import { Bossurls } from '@/app/Types/Boss';
+import Backpack from '../Backpack';
+import Console from '../Console';
 interface  Mainhandlerprops{
 //indicator
 level:number;
@@ -17,14 +19,23 @@ sound: boolean;
 setSound: React.Dispatch<React.SetStateAction<boolean>>;
 leave: () => void;
 //fight stats boss and player
-Player:Player
+P:Player
+//controll the xp leveling up
+xp:number
 }
-export default function Mainhandler({level,volume, setVolume, sound, setSound, leave,Player }:Mainhandlerprops){
-    // Ineed here manin is player and boss to be states that always change
-    
+export default function Mainhandler({level,volume, setVolume, sound, setSound, leave,P,xp }:Mainhandlerprops){
+    // Put player and boss in changable value
+    const updatedplayerxp = {...P,stats:{...P.stats,
+    level:P.stats.level+xp,attack:P.stats.attack+xp,speed:P.stats.speed+xp}}
+    const [Player,setPlayer]=useState<Player>(updatedplayerxp);
+    const [Boss,setBoss]=useState<Player>(BOSSES[level]);
     //title variables
     const [fightindicator,setFightinficator]=useState<boolean>(false);
     const [testwinner,setTesetwinner]=useState<string>('');
+    
+    
+    
+    
     //actions logic + variables
     const [turn,setTurn]=useState<boolean>(true); //always player hit first
 
@@ -45,11 +56,24 @@ export default function Mainhandler({level,volume, setVolume, sound, setSound, l
 
         return false;
     }
+
+
+    //backpacklogic + variables
+    const[quant,setQuant]=useState<number>(2); //each fight will have 2 potion to heal
+    const heal=()=>{
+
+    }
+    //console text logic +vairables;
+    const [consoletxt,setConsoletxt]=useState<string>('');
+    const consoleupdater=()=>{
+
+    }
+    
     return(
         <div className={styles.container}>
 
             <div className={styles.header}>
-                <div className={styles.midscreentop}><Title text={BOSSES[level].name} winornot={testwinner} endedfight={fightindicator}/></div>
+                <div className={styles.midscreentop}><Title text={Boss.name} winornot={testwinner} endedfight={fightindicator}/></div>
                 <div className={styles.righttopscrenn}><Pause volume={volume} setVolume={setVolume} sound={sound} setSound={setSound} leave={leave} /></div>
             </div>
 
@@ -62,13 +86,18 @@ export default function Mainhandler({level,volume, setVolume, sound, setSound, l
                 <Actions turn={turn}  setTurn={setTurn} Attack={attack} Parry={parry} Dodge={doge} />
                 </div>
                 <div className={styles.rightsectionboss}>
-                <Playerstats Player={BOSSES[level]} picture={Bossurls[level]} />
+                <Playerstats Player={Boss} picture={Bossurls[level]} />
                 </div>
             </div>
 
 
             <div className={styles.about}>
-
+                <div className={styles.leftdown}>
+                <Backpack setQuant={setQuant} quant={quant} heal={heal}/>
+                </div>
+                 <div className={styles.rightdown}>
+                <Console text={consoletxt}/>
+                 </div>
             </div>
         </div>
     )
